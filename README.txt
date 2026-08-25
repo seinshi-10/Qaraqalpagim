aa9c381e7b28d8139bbf65aca3044b1799ca24d9
seinshi-10 <tazabaevmaman18@gmail.com>
2026-08-25 23:23:16 +0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+Qaraqalpagim redesigned
+
+- Menu is now a TopStop-style card grid using dish photos and menu prices.
+- Added Qaraqalpaq / Russian / English / Uzbek language switcher.
+- Added a more distinctive heritage-inspired visual system and circular logo treatment.
+- Original menu images are preserved in assets/menu-01.jpg ... assets/menu-19.jpg.
+- Dish card data is stored in menu-data.js.
---
+++ menu-data.js
@@ -0,0 +1,492 @@
+window.MENU_ITEMS=[
+  {
+    "id": 1,
+    "category": "Уйгурские блюда",
+    "name": "Манпар",
+    "price": "18 000",
+    "image": "assets/dish-lagman.jpg"
+  },
+  {
+    "id": 2,
+    "category": "Уйгурские блюда",
+    "name": "Уйгурский лагман из говядины",
+    "price": "22 000",
+    "image": "assets/dish-lagman.jpg"
+  },
+  {
+    "id": 3,
+    "category": "Уйгурские блюда",
+    "name": "Жареный лагман из говядины",
+    "price": "22 000",
+    "image": "assets/dish-lagman.jpg"
+  },
+  {
+    "id": 4,
+    "category": "Уйгурские блюда",
+    "name": "Гурул лагман",
+    "price": "25 000",
+    "image": "assets/dish-lagman.jpg"
+  },
+  {
+    "id": 5,
+    "category": "Уйгурские блюда",
+    "name": "Ачирм сай из говядины",
+    "price": "25 000",
+    "image": "assets/dish-hot.jpg"
+  },
+  {
+    "id": 6,
+    "category": "Уйгурские блюда",
+    "name": "Гушт сай из говядины",
+    "price": "30 000",
+    "image": "assets/dish-hot.jpg"
+  },
+  {
+    "id": 7,
+    "category": "Уйгурские блюда",
+    "name": "Товук сай из куриного филе",
+    "price": "26 000",
+    "image": "assets/dish-hot.jpg"
+  },
+  {
+    "id": 8,
+    "category": "Уйгурские блюда",
+    "name": "Дапанжи из курицы",
+    "price": "35 000",
+    "image": "assets/dish-hot.jpg"
+  },
+  {
+    "id": 9,
+    "category": "Уйгурские блюда",
+    "name": "Сокоро из говядины",
+    "price": "40 000",
+    "image": "assets/dish-manty.jpg"
+  },
+  {
+    "id": 10,
+    "category": "Уйгурские блюда",
+    "name": "Манты из говядины/баранины",
+    "price": "20 000",
+    "image": "assets/dish-manty.jpg"
+  },
+  {
+    "id": 11,
+    "category": "Салаты",
+    "name": "Карақалпағым",
+    "price": "25 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 12,
+    "category": "Салаты",
+    "name": "Китайский",
+    "price": "18 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 13,
+    "category": "Салаты",
+    "name": "Греческий",
+    "price": "18 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 14,
+    "category": "Салаты",
+    "name": "Цезарь",
+    "price": "17 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 15,
+    "category": "Салаты",
+    "name": "Малибу",
+    "price": "17 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 16,
+    "category": "Салаты",
+    "name": "Жареные грибы",
+    "price": "25 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 17,
+    "category": "Салаты",
+    "name": "Оливье",
+    "price": "16 000",
+    "image": "assets/dish-salad.jpg"
+  },
+  {
+    "id": 18,
+    "category": "Салаты",
+    "name": "Свежий",
+    "price": "13 500",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 19,
+    "category": "Салаты",
+    "name": "Ачук чучук",
+    "price": "13 500",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 20,
+    "category": "Салаты",
+    "name": "Французский",
+    "price": "15 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 21,
+    "category": "Салаты",
+    "name": "Американский винегрет",
+    "price": "15 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 22,
+    "category": "Салаты",
+    "name": "Требуха с овощами",
+    "price": "25 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 23,
+    "category": "Салаты",
+    "name": "Камелия",
+    "price": "17 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 24,
+    "category": "Салаты",
+    "name": "Фантазия",
+    "price": "16 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 25,
+    "category": "Салаты",
+    "name": "Рафаэлло",
+    "price": "17 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 26,
+    "category": "Салаты",
+    "name": "Фруктовый салат",
+    "price": "23 000",
+    "image": "assets/dish-fruit.jpg"
+  },
+  {
+    "id": 27,
+    "category": "Салаты",
+    "name": "Овощное ассорти",
+    "price": "20 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 28,
+    "category": "Салаты",
+    "name": "Фруктовый ассорти",
+    "price": "60 000",
+    "image": "assets/dish-fruit.jpg"
+  },
+  {
+    "id": 29,
+    "category": "Салаты",
+    "name": "Бавария",
+    "price": "18 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 30,
+    "category": "Салаты",
+    "name": "Сосновый бар",
+    "price": "18 000",
+    "image": "assets/dish-salad2.jpg"
+  },
+  {
+    "id": 31,
+    "category": "Первые блюда",
+    "name": "Нохат суп",
+    "price": "20 000",
+    "image": "assets/dish-soup.jpg"
+  },
+  {
+    "id": 32,
+    "category": "Первые блюда",
+    "name": "Пельмени",
+    "price": "17 000",
+    "image": "assets/dish-soup.jpg"
+  },
+  {
+    "id": 33,
+    "category": "Первые блюда",
+    "name": "Борщ",
+    "price": "8 000",
+    "image": "assets/dish-soup.jpg"
+  },
+  {
+    "id": 34,
+    "category": "Вторые блюда",
+    "name": "Хан кебаб",
+    "price": "45 000",
+    "image": "assets/dish-national.jpg"
+  },
+  {
+    "id": 35,
+    "category": "Вторые блюда",
+    "name": "Казан кебаб (баранина)",
+    "price": "125 000",
+    "image": "assets/dish-national.jpg"
+  },
+  {
+    "id": 36,
+    "category": "Вторые блюда",
+    "name": "Дымгоза",
+    "price": "100 000",
+    "image": "assets/dish-national.jpg"
+  },
+  {
+    "id": 37,
+    "category": "Вторые блюда",
+    "name": "Рыба пестрый",
+    "price": "100 000",
+    "image": "assets/dish-national.jpg"
+  },
+  {
+    "id": 38,
+    "category": "Шашлыки",
+    "name": "Рыбный шашлык",
+    "price": "13 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 39,
+    "category": "Шашлыки",
+    "name": "Карақалпағым шашлык",
+    "price": "13 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 40,
+    "category": "Шашлыки",
+    "name": "Кусковой (говядина)",
+    "price": "13 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 41,
+    "category": "Шашлыки",
+    "name": "Кусковой (баранина)",
+    "price": "13 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 42,
+    "category": "Шашлыки",
+    "name": "Фарш (иждуван)",
+    "price": "10 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 43,
+    "category": "Шашлыки",
+    "name": "Рулет",
+    "price": "12 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 44,
+    "category": "Шашлыки",
+    "name": "Печень",
+    "price": "10 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 45,
+    "category": "Шашлыки",
+    "name": "Окорочка",
+    "price": "12 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 46,
+    "category": "Шашлыки",
+    "name": "Вырезка",
+    "price": "17 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 47,
+    "category": "Шашлыки",
+    "name": "Овощной",
+    "price": "10 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 48,
+    "category": "Шашлыки",
+    "name": "Кавказский фарш",
+    "price": "50 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 49,
+    "category": "Шашлыки",
+    "name": "Королевский кусковой",
+    "price": "80 000",
+    "image": "assets/dish-shashlik.jpg"
+  },
+  {
+    "id": 50,
+    "category": "Напитки",
+    "name": "Кола / Фанта / Спрайт 1,5 л",
+    "price": "15 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 51,
+    "category": "Напитки",
+    "name": "Кола / Фанта / Спрайт 1 л",
+    "price": "10 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 52,
+    "category": "Напитки",
+    "name": "Кола / Фанта / Спрайт 0,5 л",
+    "price": "5 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 53,
+    "category": "Напитки",
+    "name": "Минеральная вода 1,5 л",
+    "price": "5 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 54,
+    "category": "Напитки",
+    "name": "Сок «Долина»",
+    "price": "13 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 55,
+    "category": "Напитки",
+    "name": "Сок Viko",
+    "price": "15 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 56,
+    "category": "Напитки",
+    "name": "Айран",
+    "price": "10 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 57,
+    "category": "Напитки",
+    "name": "Фреш 0,5 / 1 л",
+    "price": "20 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 58,
+    "category": "Напитки",
+    "name": "Энергетик (баночный)",
+    "price": "12 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 59,
+    "category": "Напитки",
+    "name": "Тархун 0,33 л",
+    "price": "7 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 60,
+    "category": "Напитки",
+    "name": "Тархун 0,5 л",
+    "price": "10 000",
+    "image": "assets/dish-drinks.jpg"
+  },
+  {
+    "id": 61,
+    "category": "Горячие напитки",
+    "name": "Чай зелёный / чёрный",
+    "price": "2 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 62,
+    "category": "Горячие напитки",
+    "name": "Чай с лимоном",
+    "price": "8 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 63,
+    "category": "Горячие напитки",
+    "name": "Чай чёрный с молоком",
+    "price": "6 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 64,
+    "category": "Горячие напитки",
+    "name": "Кофе 3в1",
+    "price": "3 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 65,
+    "category": "Горячие напитки",
+    "name": "Кофе чёрный",
+    "price": "3 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 66,
+    "category": "Горячие напитки",
+    "name": "Лимон",
+    "price": "5 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 67,
+    "category": "Горячие напитки",
+    "name": "Сахар",
+    "price": "3 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 68,
+    "category": "Горячие напитки",
+    "name": "Хлеб лепешка",
+    "price": "3 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 69,
+    "category": "Горячие напитки",
+    "name": "Хлеб буханка",
+    "price": "2 000",
+    "image": "assets/dish-tea.jpg"
+  },
+  {
+    "id": 70,
+    "category": "Горячие напитки",
+    "name": "Хлебное ассорти",
+    "price": "10 000",
+    "image": "assets/dish-tea.jpg"
+  }
+];
